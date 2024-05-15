0f8596392e98bce186992eb1b387535b469a98a3
JAL212 <167903439+JAL212@users.noreply.github.com>
2024-05-15 12:43:07 -0400

    JS coding tests

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+var A = "This is a string variable"; //This is a window alert test.//
+window.alert(A);
+
+window.alert("Piglet shivered and looked at Pooh, \"H\-H\-H\-effalumps!?\", she cried."); //This is a test of escaping quotation characters//
+
+document.write("This is my document write test!" + " But this is my concatenate test. "); //These are some document writing tests.//
+
+var B = "And this is a" +" Concatenated" + " String. "; //This is a concatenated string variable test//
+document.write(B)
+
+var Family = "The Simpsons", Dad = "Homer", Mom = "Marge", Daughter = "Lisa", Son = "Bart", Baby ="Maggie" //This is a multi-variable test. I also experimented with adding a spaces and other text to a printed variable//
+
+document.write("My favorite member of the ");
+document.write(Family);
+document.write(" is ")
+document.write(Mom);
+document.write(". ");
+
+var Sent1 = "This is the begining of the string" //This is a test of concatenanted variables//
+var Sent2 = " and this is the end of the string. "
+document.write(Sent1)
+document.write(Sent2)
+
+document.write(3+3); //This is an expression test//
