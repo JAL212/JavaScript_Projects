118baf9f42770c8143b5eafa5a42ce186a04a531
JAL212 <167903439+JAL212@users.noreply.github.com>
2024-05-15 21:15:52 -0400

    Functions project documents

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function My_First_Function() {      //This is a button function test//
+    var str ="This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+function myFunction() {     //This is a concatenate function test//
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
