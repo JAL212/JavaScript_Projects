728dfc98ec46e10214f47133cee2f5ccd84cf3e0
JAL212 <167903439+JAL212@users.noreply.github.com>
2024-05-20 21:34:14 -0400

    Added comments

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -1,4 +1,4 @@
-var Z = 10;
+var Z = 10; //Global Variable practice//
 function Add_numbers_3() {
     document.write(20 + Z + "<br>");
 }
@@ -8,7 +8,7 @@ function Add_numbers_4() {
 Add_numbers_3();
 Add_numbers_4();
 
-function Add_numbers_1() {
+function Add_numbers_1() { //Local Variable & Intentional false statement that wont show a result, to use console.log to debug//
     var X = 10;
     document.write(20 + X + "<br>");
 }
@@ -18,7 +18,7 @@ function Add_numbers_2() {
 Add_numbers_1();
 Add_numbers_2();
 
-function Time_function() {
+function Time_function() { //IF Else Time statement//
     var Time = new Date().getHours();
     var Reply;
     if (Time < 12 == Time > 0) {
