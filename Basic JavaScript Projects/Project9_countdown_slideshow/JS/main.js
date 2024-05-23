83212d4b2f7ec35835121d3b16473493c4089a13
JAL212 <167903439+JAL212@users.noreply.github.com>
2024-05-23 12:33:39 -0400

    Countdown and Slideshow

---
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,14 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() { //Implementing the W3 School approach with setTimeout and slidesIndex because I was getting really hung up on the Countdown() function //
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
